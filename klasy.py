a26dc06d74cc6c9300ab6b025544442766632fca
Krzysztof Twardowski <u22eKrzTwa@liceum.p.lodz.pl>
2024-11-09 19:01:22 +0100

    Naprawa sprawdzania przecinania się prostych, usunięcie niepotzebnego kodu

--- klasy.py
+++ klasy.py
@@ -1,29 +1,23 @@
 from __future__ import annotations
 
-
 class Punkt:
     def __init__(self, x: float, y: float):
         self.x = x
         self.y = y
-    
+
 
 class Prosta:
     def __init__(self, a: float, b: float, c: float):
         if (a, b) == (0, 0):
             raise ValueError("a i b nie mogą jednocześnie wynosić 0")
-        if a == 0:
-            self.a = a/b
-            self.b = 1
-            self.c = c/b
-        else:
-            self.a = 1
-            self.b = b/a
-            self.c = c/a
+        self.a = a
+        self.b = b
+        self.c = c
 
     def punk_przecięcia(self, prosta2: Prosta) -> Punkt | bool:
         W = (self.a*prosta2.b)-(prosta2.a*self.b)
         Wx = -(self.c*prosta2.b)+(prosta2.c*self.b)
-        Wy = -(self.a*prosta2.c)+(self.a*prosta2.c)
+        Wy = -(self.a*prosta2.c)+(prosta2.a*self.c)
 
         if W == 0:
             return (Wx, Wy) == (0, 0)
@@ -50,21 +44,15 @@ class Odcinek:
     
     def przecina(self, odcinek2: Odcinek) -> bool:
         punkt_przecięcia = self.prosta.punk_przecięcia(odcinek2.prosta)
-
-        # Proste nie są równoległe, przecinają się w jednym punkcie
         if isinstance(punkt_przecięcia, Punkt):
             return all((
                 self.początek.x <= punkt_przecięcia.x <= self.koniec.x,
                 odcinek2.początek.x <= punkt_przecięcia.x <= odcinek2.koniec.x
             ))
-        
-        # Oba odcinki należą do tej samej prostej
         elif punkt_przecięcia:
             if self.prosta.b == 0:
                 return self.początek.y <= odcinek2.koniec.y and odcinek2.początek.y <= self.koniec.y
             else:
                 return self.początek.x <= odcinek2.koniec.x and odcinek2.początek.x <= self.koniec.x
-        
-        # Proste, do których należą odcinki, się nie przecinają
         else:
             return False
