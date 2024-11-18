0e3eb1b48064fe9dfe5093fa6e4fdc97ba3118cb
Krzysztof Twardowski <u22eKrzTwa@liceum.p.lodz.pl>
2024-11-18 17:39:34 +0100

    dodanie przykładów użycia do readme

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
-from math import sin, cos, pi
-from klasy import Wielokąt, Odcinek, Punkt
+from klasy import Wielokąt, Odcinek
 from time import perf_counter_ns
 
 if __name__ == "__main__":
+    # sprawdzanie przecinania się odcinków
+    odcinek1 = Odcinek((135, 126), (470, 417))
+    odcinek2 = Odcinek((513, 213), (265, 364))
+    start_czy_przecina = perf_counter_ns()
+    print(f"czy odcinki się przecinają: {odcinek1.przecina(odcinek2)}")
+    koniec_czy_przecina = perf_counter_ns()
+    print(f"sprawdzanie zajęło {(koniec_czy_przecina-start_czy_przecina)/1000000}ms")
+
+    # sprawdzanie przynależności punktu do wielokąta wypukłego
     wielokąt = Wielokąt.generuj_foremny(
         ilość_boków=10_000,
         środek=(10, 10),
@@ -19,12 +27,3 @@ if __name__ == "__main__":
     print(f"czy punkt należy do wielokąta: {wielokąt.czy_zawiera_punkt(punkt)}")
     koniec_przynależność_punktu = perf_counter_ns()
     print(f"sprawdzanie zajęło {(koniec_przynależność_punktu - start_przynależność_punktu)/1000000}ms")
-
-
-    odcinek1 = Odcinek((135, 126), (470, 417))
-    odcinek2 = Odcinek((513, 213), (265, 364))
-    
-    start_czy_przecina = perf_counter_ns()
-    print(f"czy odcinki się przecinają: {odcinek1.przecina(odcinek2)}")
-    koniec_czy_przecina = perf_counter_ns()
-    print(f"sprawdzanie zajęło {(koniec_czy_przecina-start_czy_przecina)/1000000}ms")
