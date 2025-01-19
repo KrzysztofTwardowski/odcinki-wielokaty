d8c14fac1f25a8bb75b20d280cbd3183ff00d4cb
Krzysztof <u22ekrztwa@liceum.p.lodz.pl>
2025-01-19 17:57:41 +0100

    usunięcie niepotzebnego kodu

--- klasy.py
+++ klasy.py
@@ -1,7 +1,9 @@
 from __future__ import annotations
-from math import sin, cos, pi
 
 Punkt = tuple[float, float]
+
+def iloczyn_wektorowy(w1, w2):
+    return w1[0] * w2[1] - w1[1] * w2[0]
 
 class Prosta:
     def __init__(self, a: float, b: float, c: float):
@@ -37,27 +39,22 @@ class Odcinek:
             self.początek = początek
             self.koniec = koniec
 
-        self._prosta = None
     
-    @property
     def prosta(self):
-        if self._prosta is not None:
-            return self._prosta
         a = self.początek[1] - self.koniec[1]
         b = self.koniec[0] - self.początek[0]
         c = -(a*self.początek[0] + b*self.początek[1])
-        self._prosta = Prosta(a, b, c)
-        return self._prosta
+        return Prosta(a, b, c)
 
     def przecina(self, odcinek2: Odcinek) -> bool:
-        punkt_przecięcia = self.prosta.punk_przecięcia(odcinek2.prosta)
+        punkt_przecięcia = self.prosta().punk_przecięcia(odcinek2.prosta())
         if isinstance(punkt_przecięcia, tuple):
             return all((
                 self.początek[0] <= punkt_przecięcia[0] <= self.koniec[0],
                 odcinek2.początek[0] <= punkt_przecięcia[0] <= odcinek2.koniec[0]
             ))
         elif punkt_przecięcia:
-            if self.prosta.b == 0:
+            if self.prosta().b == 0:
                 return all((
                     self.początek[1] <= odcinek2.koniec[1],
                     odcinek2.początek[1] <= self.koniec[1]
@@ -75,72 +72,36 @@ class Wielokąt:
         if len(wierzchołki) < 3:
             raise ValueError(f"Wielokąt musi mieć conajmniej 3 wierzchołki")
         self.wierzchołki = list(wierzchołki)
-        self.ilość_boków = len(self.wierzchołki)
-        self._wypukły = None
+        self.wypukły = self.czy_wypukły()
     
-    @property
-    def wypukły(self):
-        if self._wypukły is not None:
-            return self._wypukły
-        strona = 0
-        for indeks in range(self.ilość_boków):
-            bok = (
-                self.wierzchołki[indeks],
-                self.wierzchołki[(indeks+1)%self.ilość_boków]
-            )
-            prosta = Odcinek(*bok).prosta
-            nast_punkt = self.wierzchołki[(indeks+2)%self.ilość_boków]
-            if prosta.b == 0:
-                nowa_strona = (bok[1][1] - bok[0][1]) *\
-                                ((-prosta.c/prosta.a)-nast_punkt[0])
+    def czy_wypukły(self):
+        kierunek = 0
+        for indeks in range(len(self.wierzchołki)):
+            punkt1 = self.wierzchołki[indeks]
+            punkt2 = self.wierzchołki[(indeks+1)%len(self.wierzchołki)]
+            punkt3 = self.wierzchołki[(indeks+2)%len(self.wierzchołki)]
+            wektor1 = (punkt2[0]-punkt1[0], punkt2[1]-punkt1[1])
+            wektor2 = (punkt3[0]-punkt2[0], punkt3[1]-punkt2[1])
+            iloczyn = iloczyn_wektorowy(wektor1, wektor2)
+            if kierunek == 0:
+                kierunek = iloczyn
             else:
-                nowa_strona = (bok[1][0] - bok[0][0]) *\
-                                (nast_punkt[1] + (prosta.a*nast_punkt[0]+prosta.c)/prosta.b)
-            if nowa_strona == 0:
-                continue
-            elif strona == 0:
-                strona = nowa_strona
-            elif strona * nowa_strona < 0:
-                self._wypukły = False
-                return self._wypukły
-        self._wypukły = True
-        return self._wypukły
-
-    def czy_zawiera_punkt(self, punkt: Punkt) -> bool:
-        if not self.wypukły:
-            raise ValueError("Wielokąt musi być wypukły")
-        for indeks in range(self.ilość_boków):
-            prosta = Odcinek(
-                self.wierzchołki[indeks],
-                self.wierzchołki[(indeks+1)%self.ilość_boków]
-            ).prosta
-            nast_punkt = self.wierzchołki[(indeks+2)%self.ilość_boków]
-            if prosta.b == 0:
-                x_prostej = -prosta.c / prosta.a
-                if (x_prostej - nast_punkt[0]) == 0:
-                    continue
-                elif (x_prostej - nast_punkt[0]) * (x_prostej - punkt[0]) >= 0:
-                    continue
-                else:
-                    return False
-            else:
-                if (prosta.a*nast_punkt[0] + prosta.b*nast_punkt[1] + prosta.c) == 0:
-                    continue
-                elif (prosta.a*punkt[0] + prosta.b*punkt[1] + prosta.c) *\
-                     (prosta.a*nast_punkt[0] + prosta.b*nast_punkt[1] + prosta.c) >= 0:
-                    continue
-                else:
+                if kierunek * iloczyn < 0:
                     return False
         return True
 
-    @classmethod
-    def generuj_foremny(cls, ilość_boków: int, środek: Punkt, promień: float):
-        kąty = [2*pi/ilość_boków*i for i in range(ilość_boków)]
-        wierzchołki = []
-
-        for kąt in kąty:
-            x = promień * sin(kąt) + środek[0]
-            y = promień * cos(kąt) + środek[1]
-            wierzchołki.append((x, y))
-
-        return cls(*wierzchołki)
+    def czy_zawiera_punkt(self, punkt: Punkt) -> bool:
+        kierunek = 0
+        for indeks in range(len(self.wierzchołki)):
+            punkt1 = self.wierzchołki[indeks]
+            punkt2 = self.wierzchołki[(indeks+1)%len(self.wierzchołki)]
+            punkt3 = punkt
+            wektor1 = (punkt2[0]-punkt1[0], punkt2[1]-punkt1[1])
+            wektor2 = (punkt3[0]-punkt2[0], punkt3[1]-punkt2[1])
+            iloczyn = iloczyn_wektorowy(wektor1, wektor2)
+            if kierunek == 0:
+                kierunek = iloczyn
+            else:
+                if kierunek * iloczyn < 0:
+                    return False
+        return True
--- main.py
+++ main.py
@@ -1,29 +1,12 @@
 from klasy import Wielokąt, Odcinek
-from time import perf_counter_ns
 
 if __name__ == "__main__":
     # sprawdzanie przecinania się odcinków
-    odcinek1 = Odcinek((135, 126), (470, 417))
-    odcinek2 = Odcinek((513, 213), (265, 364))
-    start_czy_przecina = perf_counter_ns()
+    odcinek1 = Odcinek((-5, 0), (0, 10))
+    odcinek2 = Odcinek((-4, 10), (3, -1))
     print(f"czy odcinki się przecinają: {odcinek1.przecina(odcinek2)}")
-    koniec_czy_przecina = perf_counter_ns()
-    print(f"sprawdzanie zajęło {(koniec_czy_przecina-start_czy_przecina)/1000000}ms")
 
     # sprawdzanie przynależności punktu do wielokąta wypukłego
-    wielokąt = Wielokąt.generuj_foremny(
-        ilość_boków=50_000,
-        środek=(10, 10),
-        promień=100
-    )
+    wielokąt = Wielokąt((10, 10), (100, 10), (100, 100), (10, 100))
     punkt = (50, 50)
-
-    start_czy_wypukły = perf_counter_ns()
-    print(f"czy wypukły: {wielokąt.wypukły}")
-    koniec_czy_wypukły = perf_counter_ns()
-    print(f"sprawdzanie zajęło {(koniec_czy_wypukły - start_czy_wypukły)/1000000}ms")
-
-    start_przynależność_punktu = perf_counter_ns()
     print(f"czy punkt należy do wielokąta: {wielokąt.czy_zawiera_punkt(punkt)}")
-    koniec_przynależność_punktu = perf_counter_ns()
-    print(f"sprawdzanie zajęło {(koniec_przynależność_punktu - start_przynależność_punktu)/1000000}ms")
